039ab235f5b9971bbf67ebed8c6867720248ed70
Sebastian Oliver <seb.oliver@live.com>
2024-12-06 13:03:05 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+// const nextConfig: NextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
 
 module.exports = {
   output: "standalone",
   images: {
     domains: ["tailwindui.com"],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  experimental: {
+    appDir: true,
   },
 };
