7c38349a3f79ee40c30255b1933eea7937c4e8d4
Sebastian Oliver <seb.oliver@live.com>
2024-12-06 13:24:00 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,8 @@ module.exports = {
   images: {
     domains: ["tailwindui.com"],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
   experimental: {
     appDir: true,
+    runtime: "edge",
   },
 };
